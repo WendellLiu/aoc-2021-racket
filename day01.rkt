b8b00fd64f2ca82443f12d5ab992687e47b656cb
Wendell Liu <cuk.bas@gmail.com>
2021-12-17 18:04:38 +0800

    chore: define local variable

--- day01.rkt
+++ day01.rkt
@@ -2,10 +2,12 @@
 
 (define nums (map string->number (file->lines "inputs/day01.txt")))
 
-(car (foldl (lambda (cV acc)
-         (if (> cV (cdr acc)) 
-           (cons (+ (car acc) 1) cV)
-           (cons (car acc)  cV)
+(car (foldl
+       (lambda (cV acc)
+         (define lastValue (cdr acc))
+         (define count (car acc))
+         (if (> cV lastValue) 
+           (cons (+ count 1) cV)
+           (cons count  cV)
            )) 
-       '(0 . +inf.0) nums)
-)
+       '(0 . +inf.0) nums))
