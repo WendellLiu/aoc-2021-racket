aaf8708bf377bec12a5e528be603ff7a6a58462f
Wendell Liu <cuk.bas@gmail.com>
2021-12-18 12:03:58 +0800

    feat: day02 preprocessing

--- day02.rkt
+++ day02.rkt
@@ -3,7 +3,7 @@
 ; processing input
 (define (split-command command)
   (let ([ref-commend-list ((curry list-ref) (string-split command))])
-    (cons (ref-commend-list 0) (ref-commend-list 1))
+    (cons (ref-commend-list 0) (string->number (ref-commend-list 1)))
     ))
 (define commands (map split-command (file->lines "inputs/day02.txt")))
 
@@ -16,6 +16,6 @@
 ;(cons (+ count 1) cV)
 ;(cons count  cV)
 ;))
-;'(0 . +inf.0) commands)
+;'(0 . 0) commands)
 
 ;second part
